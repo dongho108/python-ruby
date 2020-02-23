8840651d60b103fd2fc38d6d6a70c2d3219a65f5
K_Boan <54317630+dongho108@users.noreply.github.com>
2020-02-23 17:55:47 +0900

    python&ruby_day 4

---
+++ Comment/5.rb
@@ -0,0 +1,11 @@
+
+real_egoing = 11
+#real_k8805 = "ab"
+input = "ab"
+if input == real_egoing
+  puts("Hello! "+ "egoing")
+#elsif input == real_k8805
+#  puts("Hello! "+ "k8805")
+else
+  puts("who are you")
+end
---
+++ Container/1.rb
@@ -0,0 +1,7 @@
+names = ['egoing','leezche','graphittie']
+puts(names.class)
+puts(names[0])
+print(names)
+print("\n")
+names[0] = 'k8805'
+print(names)
