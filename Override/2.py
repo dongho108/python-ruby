158c34063fc8415310b27134994b329e62318728
K_Boan <54317630+dongho108@users.noreply.github.com>
2020-03-05 20:36:11 +0900

    day 9 / finish

---
+++ Override/2.py
@@ -0,0 +1,50 @@
+class Cal(object):
+    _history = []
+    def __init__(self, v1, v2):
+        if isinstance(v1, int):
+            self.v1 = v1
+        if isinstance(v2, int):
+            self.v2 = v2
+    def getValue(self):
+        return self.v1
+        return self.v2
+    def setV1(self, v):
+        if isinstance(v, int):
+            self.v1 = v
+    def getV1():
+        return self.v1
+    def add(self):
+        result = self.v1 + self.v2
+        Cal._history.append("add : %d+%d=%d" % (self.v1, self.v2, result))
+        return result
+    def substract(self):
+        result = self.v1 - self.v2
+        Cal._history.append("substract : %d-%d=%d" % (self.v1, self.v2, result))
+        return result
+    @classmethod
+    def history(cls):
+        for item in Cal._history:
+            print(item)
+    def info(self):
+        return "Cal => v1 : %d, v2 : %d" % (self.v1, self.v2)
+class CalMultiply(Cal):
+    def multiply(self):
+        result = self.v1 * self.v2
+        Cal._history.append("multiply : %d*%d=%d" % (self.v1, self.v2, result))
+        return result
+    def info(self):
+        return "CalMultiply => %s" % super().info()
+class CalDivide(CalMultiply):
+    def divide(self):
+        result = self.v1 / self.v2
+        Cal._history.append("divide : %d/%d=%d" % (self.v1, self.v2, result))
+        return result
+    def info(self):
+        return "CalDivide => %s" % super().info()
+
+c0 = Cal(30, 60)
+print(c0.info())
+c1 = CalMultiply(10, 10)
+print(c1.info())
+c2 = CalDivide(20,10)
+print(c2.info())
